5a9390c350318559bac77ddd4c9c442c0a30c803
TardisKing40 <tardisking40@gmail.com>
2026-05-07 22:49:33 +1000

    fixed ohmyzsh

--- home/user/shell.nix
+++ home/user/shell.nix
@@ -2,7 +2,7 @@
     programs = {
         zsh = {
             enable = true;
-            ohMyZsh.enable = true;
+            oh-my-zsh.enable = true;
             shellAliases = {
                 ls = "ls -la --color=auto";
                 rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles/";
