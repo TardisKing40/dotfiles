262cb959c88acda646d8c35a8d11b181a913b25d
Twodis <tardisking40@gmail.com>
2026-05-11 17:54:44 +1000

    fix catppuccin theme

--- flake.nix
+++ flake.nix
@@ -4,24 +4,19 @@
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-unstable";
         home-manager.url = "github:nix-community/home-manager";
-	catppuccin.url = "github:catppuccin/nix";
     };
 
-    outputs = { self, nixpkgs, catppuccin, home-manager, ... }: {
+    outputs = { self, nixpkgs, home-manager, ... }: {
         nixosConfigurations.twodis-nix = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
                 ./configuration.nix
-		catppuccin.nixosModules.catppuccin
                 home-manager.nixosModules.home-manager
                 {
                     home-manager = {
                         useGlobalPkgs = true;
                         useUserPackages = true;
-                        users.twodis.imports = [
-				./home
-				catppuccin.homeModules.catppuccin
-			];
+                        users.twodis = import ./home;
                         backupFileExtension = "backup";
                     };
                 }
--- modules/theme.nix
+++ modules/theme.nix
@@ -3,6 +3,7 @@
 {
 	environment.systemPackages = with pkgs; [
 		adwaita-icon-theme
+		catppuccin-gtk
 		glib
 		gsettings-desktop-schemas
 		nwg-look
