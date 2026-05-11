7f169df95ae4c7f2345d23fa8809f8ed9f5bf433
Twodis <tardisking40@gmail.com>
2026-05-11 14:40:41 +1000

    Added zsh theming

--- home/user/shell.nix
+++ home/user/shell.nix
@@ -2,7 +2,17 @@
     programs = {
         zsh = {
             enable = true;
-            oh-my-zsh.enable = true;
+            oh-my-zsh = {
+	    	enable = true;
+		theme = "skaro";
+		plugins = [
+			"git"
+			"npm"
+			"node"
+			"rust"
+			"history"
+		];
+	    };
             shellAliases = {
                 ls = "ls -la --color=auto";
                 rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles/";
