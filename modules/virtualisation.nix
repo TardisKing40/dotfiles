86c5b500b8ee8f3116794b2529957bbe891d4dc6
Twodis <tardisking40@gmail.com>
2026-05-16 18:51:12 +1000

    enable virtualisation

---
+++ modules/virtualisation.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+	virtualisation.spiceUSBRedirection.enable = true;
+	virtualisation.libvirtd = {
+		enable = true;
+		qemu.swtpm.enable.true
+		qemu.ovmf.enable = true
+		qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
+	};
+	environment.systemPackages = with pkgs; [
+		qemu
+		spice
+		spice-gtk
+		spice-protocol
+		virt-manager
+		virt-viewer
+		win-spice
+		win-virtio
+	];
+}
