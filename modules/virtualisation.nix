aa1fad49a6c6ac8efd4b69a61a6715917bcd4ad2
Twodis <tardisking40@gmail.com>
2026-05-16 18:56:13 +1000

    ovmf enabled by default

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -5,8 +5,6 @@
 	virtualisation.libvirtd = {
 		enable = true;
 		qemu.swtpm.enable = true;
-		qemu.ovmf.enable = true;
-		qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
 	};
 	environment.systemPackages = with pkgs; [
 		qemu
