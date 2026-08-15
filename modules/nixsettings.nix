7455c73b4886e3ecf2ac0c54abcfc17bf85bf29e
Twodis <tardisking40@gmail.com>
2026-08-16 01:03:10 +1000

    add automatic cleanup of old packages
    Signed-off-by: Twodis <twodis@twodis.xyz>

--- modules/nixsettings.nix
+++ modules/nixsettings.nix
@@ -3,4 +3,10 @@
 {
     nixpkgs.config.allowUnfree = true;
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    
+    # Automatic Cleanup
+    nix.gc.automatic = true;
+    nix.gc.dates = "daily";
+    nix.gc.options = "--delete-older-than 10d";
+    nix.settings.auto-optimise-store = "true";
 }
