fbd739043fc6457a4977ca2f1ce8dc94f3cf9ce6
TardisKing40 <twodis@twodis.xyz>
2026-08-16 04:16:24 +1000

    Added extra settings to git.nix
    Signed-off-by: Twodis <twodis@twodis.xyz>

--- home/user/git.nix
+++ home/user/git.nix
@@ -1,9 +1,13 @@
 {
     programs.git = {
         enable = true;
-        settings.user = {
-			name = "twodis";
-            email = "twodis@twodis.xyz";
-        };
+        settings = {
+		user.name = "Twodis";
+		user.email = "twodis@twodis.xyz";
+		user.signingkey = "8C3838983E0C91B397861AD8AB450ADA98AB9E0D";
+		commit.gpgsign = true;
+		gpg.format = "openpgp";
+	};
+
     };
 }
